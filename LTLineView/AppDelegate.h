a537663d0bd419c26bf6463077a119950a89e19c
liht <liht@AppledeiMac.local>
2017-07-19 17:35:33 +0800

    Initial Commit

---
+++ LTLineView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTLineView
+//
+//  Created by liht on 2017/7/19.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTLineView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTLineView
+//
+//  Created by liht on 2017/7/19.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
